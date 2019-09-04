49dd9c5d18f1ae261d4a03ac7033a246de68371d
amineabdel <amine.oostende@hotmail.com>
2019-09-04 15:55:21 +0200

    gesorteerd en parameter

--- appine/urls.py
+++ appine/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('infractions/<int:numbers>', views.infractions, name='infractions'),
     #url(r'^(?P<numbers>[\d/]+)$', views.detail)
     #path(r'<int:a>/<int:b>', views.detail, name='detail'),
     #url(r'^.*/$', views.urlke, name='urlke'),
